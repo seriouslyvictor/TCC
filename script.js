b08c6e2d8c21d379eef4caab928dbce88e21e2f4
Victor Schiavo <schiavovictor@outlook.com>
2022-07-28 15:03:55 -0300

    Added Modal Window

--- script.js
+++ script.js
@@ -2,6 +2,18 @@
 
 const itemSanfona = document.querySelectorAll('.item-sanfona');
 const iconeSanfona = document.querySelectorAll('.icone-sanfona')
+const modal = document.querySelector('.modal');
+const overlay =  document.querySelector('.overlay');
+const queroAprender = document.querySelector('.destaque');
+const btnModalFechar = document.querySelector('.modal-fechar');
+const signup = document.querySelector('.signup');
+
+
+
+queroAprender.addEventListener('click', abrirModal);
+btnModalFechar.addEventListener('click', abrirModal);
+overlay.addEventListener('click', abrirModal);
+signup.addEventListener('click', abrirModal);
 
 for (let i = 0; i < itemSanfona.length; i++) {
     itemSanfona[i].addEventListener('click', function() {
@@ -10,6 +22,13 @@ for (let i = 0; i < itemSanfona.length; i++) {
         iconeSanfona[i].classList.toggle('fa-chevron-right')
     })
 }
+
+function abrirModal() {
+    modal.classList.toggle('escondido')
+    overlay.classList.toggle('escondido')
+    document.body.classList.toggle('travar')    
+}
+
 
 
 
